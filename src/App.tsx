58a065c590553161189a39677485dc93e92cf518
Rory Smith <rory@chess.com>
2022-01-30 16:38:11 +0000

    Update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import "./index.css";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { TParsedTeam } from "./types";
-import { TableRow } from "./TableRow";
+import './index.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { TParsedTeam } from './types';
+import { TableRow } from './TableRow';
 
-export default function App() {
+export default function App () {
   const [data, setData] = useState<Array<Array<TParsedTeam> | null>>([]);
   const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     const get = async () => {
       const leagues = [
-        "premier-league",
-        // "bundesliga",
-        // "la-liga",
-        // "serie-a",
-        // "ligue-1",
+        'premier-league',
+        'bundesliga',
+        'la-liga',
+        'serie-a',
+        'ligue-1',
       ];
 
-      const requestUrlBase = "http://localhost:1338/api/cann-data";
+      const requestUrlBase = 'http://localhost:1338/api/cann-data';
+      // const requestUrlBase =
+      //   "https://serene-headland-51052.herokuapp.com/api/cann-data";
 
       try {
         axios
@@ -34,11 +36,11 @@ export default function App() {
             })
           )
           .catch((err) => {
-            console.log("ERROR:", err);
+            console.log('ERROR:', err);
             setIsError(true);
           });
       } catch (err) {
-        console.log("ERROR:", err);
+        console.log('ERROR:', err);
         setIsError(true);
       }
     };
@@ -47,23 +49,23 @@ export default function App() {
   }, []);
 
   return (
-    <div className="App">
-      <table>
-        {data.map((league, i) => {
-          return (
-            <tbody
+    <div style={{ display: 'flex' }}>
+      {data.map((league, i) => {
+        return (
+          <div style={{padding: '10px', flexGrow: 1}}>
+            <div
               key={i + 100}
               style={{
-                fontSize: "30px",
+                fontSize: '16px',
               }}
             >
-              {league?.map((team, i) => (
-                <TableRow team={team} key={i} />
-              ))}
-            </tbody>
-          );
-        })}
-      </table>
+            {league?.map((team, i) => (
+              <TableRow team={team} key={i}/>
+            ))}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- src/TableRow.tsx
+++ src/TableRow.tsx
@@ -1,5 +1,5 @@
-import { Fragment } from "react";
-import { TParsedTeam } from "./types";
+import { Fragment } from 'react';
+import { TParsedTeam } from './types';
 
 interface IProps {
   team: TParsedTeam;
@@ -7,22 +7,28 @@ interface IProps {
 
 export const TableRow: React.FC<IProps> = ({ team }) => {
   return (
-    <tr>
-      {team.map((t, i) => (
-        <Fragment key={i + 50}>
-          {i === 0 && <td>{t.points}</td>}
-          {t.name && t.logo && <TeamCell name={t.name} logo={t.logo} />}
-        </Fragment>
-      ))}
-    </tr>
+    <div style={{
+      background: team[ 0 ].name ? 'rgba(0,0,0,0.6)' : 'rgba(0,0,0,0.4)',
+      borderBottom: '1px solid rgba(255,255,255,0.05)',
+      padding: '1px 0',
+    }}>
+      <div style={{ display: 'flex' }}>
+        {team.map((t, i) => (
+          <Fragment key={i + 50}>
+            {i === 0 && <td style={{ padding: '0 30px' }}>{t.points}</td>}
+            {t.name && t.logo && <TeamCell name={t.name} logo={t.logo}/>}
+          </Fragment>
+        ))}
+      </div>
+    </div>
   );
 };
 
 const TeamCell: React.FC<{ name: string; logo: string }> = ({ name, logo }) => {
   return (
-    <td style={{ display: "flex", alignItems: "center" }}>
-      <img style={{ height: "50px" }} src={logo} />
-      <span>{name?.toUpperCase()}</span>
-    </td>
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <img style={{ height: '14px', marginRight: '10px' }} src={logo}/>
+      <span style={{ marginRight: '10px' }}>{name}</span>
+    </div>
   );
 };
